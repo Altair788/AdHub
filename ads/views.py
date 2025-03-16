17d08340cf61058ab2958d4104b4e2c19a0a78c8
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-16 20:34:04 +0300

    feat: CRUD for Ad

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from ads.models import Ad
+from ads.paginations import AdPaginator
+from ads.serializers import AdSerializer
+from users.permissions import IsAuthor, IsAdmin
+
+
+class AdCreateAPIView(generics.CreateAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    permission_classes = (
+        IsAdmin | IsAuthenticated,
+    )
+
+    def perform_create(self, serializer):
+        ad = serializer.save()
+        ad.author = self.request.user
+        ad.save()
+
+
+class AdListAPIView(generics.ListAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    pagination_class = AdPaginator
+    permission_classes = ()
+
+
+class AdRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    permission_classes = (
+        IsAuthenticated | IsAdmin,
+    )
+
+
+class AdUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    permission_classes = (
+        IsAuthenticated,
+        IsAdmin | IsAuthor,
+    )
+
+
+class AdDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    permission_classes = (
+        IsAuthenticated,
+        IsAdmin | IsAuthor,
+    )
