a0485fe05998e79ab28567688da115b046dadbef
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-16 20:36:17 +0300

    feat: add permissions for users'

--- users/permissions.py
+++ users/permissions.py
@@ -8,3 +8,24 @@ class CanViewAPI(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_staff
+
+
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь администратором.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="admins").exists()
+
+
+class IsAuthor(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь владельцем.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if obj.author== request.user:
+            return True
+        return False
